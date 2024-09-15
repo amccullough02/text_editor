3079e53fead27a772bfcf463c3a31cba37a11f4e
Alex McCullough <alexmccullough@outlook.com>
2024-09-15 16:19:50 +0100

    refactor: crossterm implemented idiomatically

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read};
+use crossterm::event::{read, Event::Key, KeyCode::Char};
 use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
 
 pub struct Editor {
@@ -9,22 +9,24 @@ impl Editor {
     pub fn default() -> Self {
         Editor{}
     }
+
     pub fn run(&self) {
         enable_raw_mode().unwrap();
-        for b in io::stdin().bytes() {
-            match b {
-                Ok(b) => {
-                    let c = b as char;
-                    if c.is_control() {
-                        println!("Binary: {0:0b} ASCII: {0:#03} \r", b)
-                    } else {
-                        println!("Binary: {0:0b} ASCII: {0:#03} Character: {1:#?} \r", b, c)
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{:?} \r", event);
+                    match event.code {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        },
+                        _ => (),
                     }
-                    if c == 'q' {
-                        break;
-                    }
-                }
+                },
                 Err(err) => println!("Error: {}", err),
+                _ => ()
             }
         }
         disable_raw_mode().unwrap();
